9efda898cbfe5b97ce5f7e1cc55f1d28cf51fe13
Josh Klingler <joshklingler@gmail.com>
2013-05-12 18:11:32 -0400

    Change "checkDraw" to "checkHit"

--- src/DealerHand.java
+++ src/DealerHand.java
@@ -11,7 +11,7 @@ public class DealerHand extends BlackjackHand
 {
     //------------------------------------------------------------------------
     // If count is 16 or below, dealer must hit. Otherwise they stay.
-    public boolean checkDraw()
+    public boolean checkHit()
     {
 	if (handValue <= 16)
 	{
