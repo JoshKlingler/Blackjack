d5aabeb1b0c53b06f2e4223a225047b857e3a991
Josh Klingler <joshklingler@gmail.com>
2013-05-12 22:07:49 -0400

    dealCards
    Added dealCards method

--- src/BlackjackGame.java
+++ src/BlackjackGame.java
@@ -1,12 +1,33 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
+ * This class runs a game of Blackjack.
  * @author Josh
  */
-public class BlackjackGame {
+public class BlackjackGame
+{
+    //------------------------------------------------------------------------
+    // Data members:
+    DealerHand dealer;
+    Hand player;
+    Deck deck;
+
+    //------------------------------------------------------------------------
+    // Default constructor
+    BlackjackGame()
+    {
+	dealer = new DealerHand();
+	player = new Hand();
+	deck   = new Deck();
+    }
+    //------------------------------------------------------------------------
+    // Deals two cards to the dealer and the player.
+    public void dealCards()
+    {
+	deck.GenerateDeck();  // Reshuffles deck for every hand
+
+	player.drawHand( deck.drawCard(), deck.drawCard() );
+	dealer.drawHand( deck.drawCard(), deck.drawCard() );
+
+	
+    }
 
 }
